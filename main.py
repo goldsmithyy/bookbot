a1dfd9118d522545602b10f23a26c64840771966
goldsmithyy <notoxist@gmail.com>
2025-09-17 10:56:29 +1000

    D: finished project

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
+from stats import get_word_count
+from stats import get_letter_count
+from stats import list_conversion
+import sys
 def get_book_text (filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
 def main():
-    from stats import get_word_count
-    file_contents = get_book_text("books/frankenstein.txt")
-    get_word_count(file_contents)  
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {sys.argv[1]}")
+        file_contents = get_book_text(f"{sys.argv[1]}")
+        print("----------- Word Count ----------")
+        get_word_count(file_contents)  
+        letter_count = get_letter_count(file_contents)
+        sorted_list = list_conversion(letter_count)
+        print("--------- Character Count -------")
+        for lists in sorted_list:
+            if lists["char"].isalpha() == True:
+                print(lists["char"] + ":", lists["num"])
+        print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -3,4 +3,21 @@ def get_word_count(file_contents):
     word_list = file_contents.split()
     for words in word_list:
         num = len(word_list)
-    print(f"{num} words found in the document")
+    print(f"Found {num} total words")
+def get_letter_count(contents):
+        letter_dict = {}
+        contents_lowercase = str.lower(contents)
+        for letter in contents_lowercase:
+             if letter not in letter_dict:
+                  letter_dict[letter] = 0
+             letter_dict[letter] += 1
+        return letter_dict
+def sort_on(letters):
+     return letters["num"]
+def list_conversion(letter_dict):
+     letters = []
+     for key in letter_dict:
+          value = letter_dict[key]
+          letters.append({"char" : key, "num" : value})
+     letters.sort(reverse=True, key=sort_on)
+     return letters
